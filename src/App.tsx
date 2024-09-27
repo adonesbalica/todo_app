88e75048d2dbb6cfb206ee7f6f5e6d0e9f7a0120
Adones Balica <adonesbalica@gmail.com>
2024-09-27 13:11:53 -0700

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { BrowserRouter } from 'react-router-dom'
+import { Router } from './Router'
+
+// 00:46:39.83
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
